a586852b362dc8ab80a310ab1a70599d73acb19b
ambericorn <66884081+ambericorn@users.noreply.github.com>
2024-05-01 00:15:19 -0400

    egg drop in pan testing

---
+++ DaBaby/Assets/Scripts/Egg_Crack_Collision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg_Crack_Collision : MonoBehaviour
+{
+    public GameObject sunnySideUpEgg;
+
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.name == "Pan"){
+            Vector3 pos = transform.position;
+            Destroy(gameObject);
+            Instantiate(sunnySideUpEgg, pos, Quaternion.identity);
+    }
+    }
+}
