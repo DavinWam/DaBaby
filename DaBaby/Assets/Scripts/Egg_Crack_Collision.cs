723b54acf5c0a56dc0a94d0f7fe83f26cb43c732
DavinW <49922027+DavinWam@users.noreply.github.com>
2024-05-02 20:30:11 -0400

    egg cracks based on velocity

--- DaBaby/Assets/Scripts/Egg_Crack_Collision.cs
+++ DaBaby/Assets/Scripts/Egg_Crack_Collision.cs
@@ -9,7 +9,7 @@ public class Egg_Crack_Collision : MonoBehaviour
     private void OnCollisionEnter(Collision collision) 
     {
         if (collision.gameObject.name == "Pan"){
-            Vector3 pos = collision.transform.position;
+            Vector3 pos = collision.transform.position - new Vector3(0.12f,0,0);
             Destroy(gameObject);
             Instantiate(sunnySideUpEgg, pos, Quaternion.identity);
     }
--- DaBaby/Assets/Scripts/Plain_Egg_Collision.cs
+++ DaBaby/Assets/Scripts/Plain_Egg_Collision.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Plain_Egg_Collision : MonoBehaviour
 {
     public GameObject crackedEgg;
+    private bool hasMadeEgg = false;
+    public float breakVelocity = .2f;
 
+    public void Update(){
+
+       
+    }
     private void OnCollisionEnter(Collision collision) 
     {
 
-        
-        if (collision.gameObject.name != "box" && collision.gameObject.name != "holder" && collision.gameObject.name != "counter" && collision.gameObject.name != "egg"){
+         Debug.Log(collision.relativeVelocity.magnitude);
+        if ( collision.relativeVelocity.magnitude > breakVelocity && !hasMadeEgg){
+            hasMadeEgg = true;
             Debug.Log(collision.gameObject.name);
-            Vector3 pos = transform.position;
+            Vector3 pos = transform.position + new Vector3(0,.02f,0);
             Destroy(gameObject);
             Instantiate(crackedEgg, pos, Quaternion.identity);
         }
